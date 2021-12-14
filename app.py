5269f3066422c04319725317f5c4130e1fdedc15
Ni4ka1991 <astahova_nica@mail.ru>
2021-12-14 10:43:01 +0200

    General information

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 
+import numpy as np
+from os import system
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+plt.style .use( 'seaborn-whitegrid' ) 
+
+
+
+#EXPERIMENT DATA
+time_m = [ 5,  10,  20,  60 ]      #boiler running time (min)
+temp_c = [ 15, 20,  23,  25 ]      #temp in the house (C) max = 100C
+
+#plt.plot( time_m, temp_c, color = "green", linestyle="solid", linewidth = 1, marker = "x")
+#plt.show()
+#
+#1. График квадратичной функции  y = sqrt(x)
+#2. max y = [0, 100]; x=[0, 10_000]
+#3. 
+
+system( "clear" )
+y = sqrt(x)
+y = w * sqrt(x) + b
+
+
+
+
 

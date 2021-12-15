6f9ef811e7b4c42d53f9e36ab37aa866a3948647
Ni4ka1991 <astahova_nica@mail.ru>
2021-12-15 14:48:58 +0200

    Some changes

--- app.py
+++ app.py
@@ -16,51 +16,33 @@ temp_c = [ 0, 15, 20,  23,  25 ]      # temp in the house (C) max = 100 C
 #plt.plot( time_m, temp_c, color = "green", linestyle="solid", linewidth = 1, marker = "x" )
 #plt.show()
 
-
-
-#CLASSIC SOLUTION #################################################################################
-#Square root function  График функции корень из x => 
-#Частный случай функции квадратного корня: y = sqrt(x - a) + b. a смещает график по оси x, так как под знаком корня должно быть положительное число (не наш случай); b - смещает график по y
-#b дает значительный прирост функции по оси y, в нашем случае "скачек" по оси y наблюдается только до значения x = 20, т.е. должна быть какая-нибудь функция от b, возможно, тоже sqrt(b) 
-
-
-#Для нашего случая:
-#1. y = [0, 100];
-#2. x = [0, 10_000]
-#3. b >= 0
-
-
-#y = w * np.sqrt( np.sqrt(x[i] ))
-
-# ##################################################################################################
-
-
-# TASK #############################################################################################
-
-# I'm comfortable with temperature of 26.5 in the house.
-#Task: How long should the boiler be on?
-
-#Questions raised:
-#1. Is the house cold? Should I take into accaunt the initial temperature of the house?
-
-###################################################################################################
-
 # GO-GO ###########################################################################################
-#a = 2
 w = 11
-b = 0
 Y = []
 
 for i in range( len( time_m )):
- #y = a * w * w * np.sqrt( np.sqrt( time_m[i] )) + b
- y = np.sin(time_m[i] *  .1 ) * w * np.sqrt( np.sqrt( time_m[i] )) + b
+ y = w * np.sqrt( np.sqrt( time_m[i] ))
  Y.append(y)
 
 system( "clear" )
 
-plt.plot( time_m, temp_c, color = "green", linestyle="solid", linewidth = 1, marker = "x" )
-plt.plot( time_m, Y, color = "red", linestyle="solid", linewidth = 1, marker = "x" )
-plt.show()
+#view data func
+def print_differences( list_lenght ):
+    print( "        temp_c       |   Y   |  ERROR  ")
+    print("  ________ ________________________________________")
+    for i in range( list_lenght ):
+        print(f"{Y[i]:20} | {temp_c[i]:5} | {abs(Y[i] - temp_c[i]):20}")
+#############
+
+
+data_quantity = len( time_m )
+print_differences( data_quantity, )
+
+
+
+#plt.plot( time_m, temp_c, color = "green", linestyle="solid", linewidth = 1, marker = "x" )
+#plt.plot( time_m, Y, color = "red", linestyle="solid", linewidth = 1, marker = "x" )
+#plt.show()
 
 
 
